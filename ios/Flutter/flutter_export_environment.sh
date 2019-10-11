b77c04d14d0e485d37f2bf9199cf3a08f93f8ce0
Jan Simson <jan.simson@three-2-one.com>
2019-10-11 10:05:50 -0400

    Fix building for iOS

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/1.7.8+hotfix.3/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/jan/Privat/Projekte/Flutter/shopping_list"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/jan/Privat/Projekte/Flutter/shopping_list/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/1.7.8+hotfix.3/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/1.7.8+hotfix.3/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
